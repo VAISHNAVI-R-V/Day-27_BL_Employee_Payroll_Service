2a495e74ab62fce5e59fbf5aeacf19dd415ac544
VAISHNAVI-R-V <vaishucv14@gmail.com>
2021-09-25 16:20:38 +0530

    ADDED: To demonstrate File Operations.

---
+++ src/main/java/com/bridgelabz/FileUtils.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+
+import java.io.File;
+
+public class FileUtils {
+    public static boolean deleteFile(File contentsToDelete) {
+        File[] allContents = contentsToDelete.listFiles();
+        if (allContents != null) {
+            for (File file : allContents) {
+                deleteFile(file);
+            }
+        }
+        return contentsToDelete.delete();
+    }
+}
